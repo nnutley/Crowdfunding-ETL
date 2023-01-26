c73b98a519da6054bb38d87f83ea4b1d754d0fa8
nnutley <nnutley@arizona.edu>
2023-01-26 10:43:40 -0800

    added backers table to db schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -44,6 +44,17 @@ CREATE TABLE "contacts" (
      )
 );
 
+CREATE TABLE "backers"(
+    "backer_id" varchar(10) NOT NULL,
+    "cf_id" int NOT NULL,
+    "first_name" varchar(50) NOT NULL,
+    "last_name" varchar(50) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+    )
+);
+
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
@@ -53,3 +64,6 @@ REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "backers" ("cf_id");
